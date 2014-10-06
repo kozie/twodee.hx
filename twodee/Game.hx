2272e43db5426e1ac9a5a7011d582b04c687c1f8
kozie <flamefingers@gmail.com>
2014-10-06 23:24:36 +0200

    Herp Derp..

--- twodee/Game.hx
+++ twodee/Game.hx
@@ -1,12 +1,45 @@
 package twodee;
 
-class Game 
-{
-	private function new() {
+import flash.events.Event;
+import openfl.display.Sprite;
 
+class Game extends Sprite
+{
+	public var gameWidth:Int;
+	public var gameHeight:Int;
+
+	public var updateRate:Float;
+	public var frameRate:Float;
+
+	public var backgroundColor:UInt;
+
+	public function new(width:Int = 800, height:Int = 600, updateRate:Float = 60.0, frameRate:Float = 60.0, backgroundColor:Uint = 0x333333) {
+		// Set window options
+		gameWidth = this.width = width;
+		gameHeight = this.height = height;
+
+		// Set update and frame rates
+		this.updateRate = updateRate;
+		this.frameRate = frameRate;
+
+		// Set color
+		this.backgroundColor = backgroundColor;
+
+		// Set initializing when added to stage
+		addEventListener(Event.ADDED_TO_STAGE, init);
 	}
 
-	public static function test():Void {
-		trace("Herp Derp");
+	private function init(_):Void {
+		if (stage == null) return;
+
+		removeEventListener(Event.ADDED_TO_STAGE, init);
+
+		// Setup stage dimension and frame rate
+		this.stage.stageWidth = this.width;
+		this.stage.stageHeight = this.height;
+		this.stage.frameRate = this.frameRate;
+
+		// Set color
+		this.stage.color = this.backgroundColor;
 	}
 }
